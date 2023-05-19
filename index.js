7913aaddb37208738889d8986e0014825c7969bc
saidur-way <saiduronway@gmail.com>
2023-05-19 14:27:21 +0600

    mongodb add and dotenv add and create route

--- index.js
+++ index.js
@@ -1,19 +1,88 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
-const port = process.env.port || 5000;
-
-const products = require('./data/products.json')
+const port = process.env.PORT || 5000;
 
 app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ie2mpcl.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+     try {
+          // Connect the client to the server	(optional starting in v4.7)
+     await client.connect();
+     const dataCollection = client.db('teddyToys').collection('toys')
+
+     app.get('/products', async(req, res)=>{
+          
+          const cursor = dataCollection.find()
+          const result = await cursor.toArray();
+          // console.log(result)
+          res.send(result)
+     })
+     app.get('/products/category', async(req, res)=>{
+
+          // const data = req.params.sub;
+          // console.log(req.query?.subcategory)
+
+          const query = {subCategory:  req.query?.subcategory }
+          console.log(query)
+          // const query = {sellerName: }
+
+          // // const query = {subCategory: toString(data)}
+          const cursor = dataCollection.find(query)
+          const result = await cursor.toArray()
+          res.send(result) 
+
+     })
+     app.get('/products/:id', async(req, res)=>{
+          const id = req.params.id
+          console.log(id)
+          // if(req.query.subCategory){
+          //      query= {subCategory: req.query.subCategory}
+          // }
+          const filter = {_id: new ObjectId(id)}
+          const result = await dataCollection.findOne(filter)
+          console.log(result)
+          res.send(result)
+     })
+     app.get('/products/:sub', async(req, res)=>{
+         
+          // const categoryName = req.params.subCategory;
+          console.log(req.params.sub)
+     
+     })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+//     await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res)=>{
      res.send('Toys Server is running!')
 })
-app.get('/toys', (req, res)=>{
-     res.send(products)
-})
+
 app.listen(port, ()=>{
      console.log(`Tyos server port: ${port}`)
 })
